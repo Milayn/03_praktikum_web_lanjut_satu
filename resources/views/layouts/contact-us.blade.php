5984395dade6b7997687d3a3a637694b61b750dd
Milayn <millayunita07@gmail.com>
2022-03-10 00:04:13 +0700

    edit href

--- resources/views/layouts/contact-us.blade.php
+++ resources/views/layouts/contact-us.blade.php
@@ -119,7 +119,7 @@
                 <!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="navbar-menu">
 					<ul class="nav navbar-nav ml-auto" data-in="fadeInDown" data-out="fadeOutUp">
-						<li class="nav-item"><a class="nav-link" href="index">Home</a></li>
+						<li class="nav-item"><a class="nav-link" href="">Home</a></li>
 						<li class="nav-item"><a class="nav-link" href="about">About Us</a></li>
 						<li class="dropdown">
 							<a href="#" class="nav-link dropdown-toggle arrow" data-toggle="dropdown">SHOP</a>
--- routes/web.php
+++ routes/web.php
@@ -56,3 +56,7 @@ Route::get('/shop', function () {
 Route::get('/wishlist', function () {
     return view('layouts/wishlist');
 });
+
+Route::get('/contact-us', function () {
+    return view('layouts/contact-us');
+});
